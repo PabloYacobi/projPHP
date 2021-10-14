9644e75473874fbadc661ae5ffb6ddf9deaa7053
PabloYacobi <pablo.yacobi101@outlook.com>
2021-10-14 18:02:34 +0200

    php

---
+++ back2.php
@@ -0,0 +1,9 @@
+<?php
+$a=0;
+$year=2006;
+echo("Has nacido en ".$year );
+
+while($a<=10){
+    echo($a);
+    $a++;
+}
--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function verify(event){
     event.preventDefault();
     console.log(event);
-
+    console.log(event.target[0].value.length)
+        if(event.target[0].value.length<3){
+            document.getElementById("userver").style.display="block";
+        }
+            else{
+            document.getElementById("userver").style.display="none";
+        }
+        if(event.target[1].value.length<3){
+            document.getElementById("passwordver").style.display="block"
+        }
+            else{
+            document.getElementById("passwordver").style.display="none";
+        }
 }
